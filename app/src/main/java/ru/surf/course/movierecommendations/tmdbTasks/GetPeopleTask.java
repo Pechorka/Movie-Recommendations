c41f619be276b4cac73d6d68883b24924cc95b8f
nadraliev <nadraliev@gmail.com>
2017-02-02 15:31:49 +0300

    movie credits json parsing properly

--- app/src/main/java/ru/surf/course/movierecommendations/tmdbTasks/GetPeopleTask.java
+++ app/src/main/java/ru/surf/course/movierecommendations/tmdbTasks/GetPeopleTask.java
@@ -4,15 +4,23 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 import ru.surf.course.movierecommendations.BuildConfig;
+import ru.surf.course.movierecommendations.models.Actor;
+import ru.surf.course.movierecommendations.models.CrewMember;
 import ru.surf.course.movierecommendations.models.People;
 
 /**
@@ -22,6 +30,17 @@ import ru.surf.course.movierecommendations.models.People;
 public class GetPeopleTask extends AsyncTask<String, Void, List<People>> {
 
     private final String API_KEY_PARAM = "api_key";
+    private final String TMDB_CAST = "cast";
+    private final String TMDB_CREW = "crew";
+    private final String TMDB_CAST_ID = "cast_id";
+    private final String TMDB_CHARACTER = "character";
+    private final String TMDB_CREDIT_ID = "credit_id";
+    private final String TMDB_ID = "id";
+    private final String TMDB_NAME = "name";
+    private final String TMDB_ORDER = "order";
+    private final String TMDB_PROFILE_PATH = "profile_path";
+    private final String TMDB_DEPARTMENT = "department";
+    private final String TMDB_JOB = "job";
     private final String LOG_TAG = getClass().getSimpleName();
 
     private Tasks task;
@@ -89,6 +108,14 @@ public class GetPeopleTask extends AsyncTask<String, Void, List<People>> {
             }
         }
 
+        try {
+            List<People> result = parseJson(peopleJsonStr);
+            return result;
+        } catch (JSONException | ParseException e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+            e.printStackTrace();
+        }
+
         return null;
     }
 
@@ -100,6 +127,22 @@ public class GetPeopleTask extends AsyncTask<String, Void, List<People>> {
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+    }
+
+    private List<People> parseJson(String jsonStr) throws JSONException, ParseException {
+
+        JSONObject jsonObject = new JSONObject(jsonStr);
+
+        List<People> result = new ArrayList<>();
+
+        switch (task) {
+            case GET_MOVIE_CREDITS:
+                result = parseMovieCredits(jsonObject);
+                break;
+        }
+
+        return result;
+
     }
 
     public void getMovieCredits(int movieId) {
@@ -116,6 +159,39 @@ public class GetPeopleTask extends AsyncTask<String, Void, List<People>> {
                 .appendPath(TMDB_CREDITS)
                 .appendQueryParameter(API_KEY_PARAM, BuildConfig.TMDB_API_KEY)
                 .build();
+    }
+
+    private List<People> parseMovieCredits(JSONObject jsonObject) throws JSONException {
+        JSONArray cast = jsonObject.getJSONArray(TMDB_CAST);
+        JSONArray crew = jsonObject.getJSONArray(TMDB_CREW);
+        List<People> result = new ArrayList<>();
+        People people;
+        for (int i = 0; i < cast.length(); i++) {
+            people = new Actor(
+                    cast.getJSONObject(i).getString(TMDB_NAME),
+                    cast.getJSONObject(i).getInt(TMDB_ID),
+                    cast.getJSONObject(i).getString(TMDB_PROFILE_PATH),
+                    cast.getJSONObject(i).getInt(TMDB_CAST_ID),
+                    cast.getJSONObject(i).getString(TMDB_CHARACTER),
+                    cast.getJSONObject(i).getString(TMDB_CREDIT_ID),
+                    cast.getJSONObject(i).getInt(TMDB_ORDER)
+            );
+            result.add(people);
+        }
+
+        for (int i = 0; i < crew.length(); i++) {
+            people = new CrewMember(
+                    crew.getJSONObject(i).getString(TMDB_NAME),
+                    crew.getJSONObject(i).getInt(TMDB_ID),
+                    crew.getJSONObject(i).getString(TMDB_PROFILE_PATH),
+                    crew.getJSONObject(i).getString(TMDB_CREDIT_ID),
+                    crew.getJSONObject(i).getString(TMDB_DEPARTMENT),
+                    crew.getJSONObject(i).getString(TMDB_JOB)
+            );
+            result.add(people);
+        }
+
+        return result;
     }
 
 
