faff55371823f38ca3991398b0d5f65058bc61d2
nadraliev <nadraliev@gmail.com>
2017-02-03 14:05:23 +0300

    expanded Person with new fields

--- app/src/main/java/ru/surf/course/movierecommendations/models/Person.java
+++ app/src/main/java/ru/surf/course/movierecommendations/models/Person.java
@@ -1,17 +1,26 @@
 package ru.surf.course.movierecommendations.models;
 
+import java.util.Date;
+
 /**
  * Created by andrew on 1/29/17.
  */
 
 public class Person {
 
+    protected enum Gender {MALE, FEMALE}
+
     protected String mName;
     protected int mId;
     protected String mProfilePath;
-
-    public Person() {
-    }
+    protected boolean mAdult;
+    protected String mBiography;
+    protected Date mBirthday;
+    protected Date mDeathday;
+    protected Gender mGender;
+    protected String mImdbId;
+    protected String mPlaceOfBirth;
+    protected Double mPopularity;
 
     public Person(String name, int id, String profilePath) {
         mName = name;
@@ -19,27 +28,105 @@ public class Person {
         mProfilePath = profilePath;
     }
 
+    public Person(String mName, int mId, String mProfilePath, boolean mAdult, String mBiography, Date mBirthday, Date mDeathday, Gender mGender, String mImdbId, String mPlaceOfBirth, Double mPopularity) {
+        this.mName = mName;
+        this.mId = mId;
+        this.mProfilePath = mProfilePath;
+        this.mAdult = mAdult;
+        this.mBiography = mBiography;
+        this.mBirthday = mBirthday;
+        this.mDeathday = mDeathday;
+        this.mGender = mGender;
+        this.mImdbId = mImdbId;
+        this.mPlaceOfBirth = mPlaceOfBirth;
+        this.mPopularity = mPopularity;
+    }
+
     public String getName() {
         return mName;
     }
 
-    public void setName(String mName) {
-        this.mName = mName;
+    public void setName(String name) {
+        this.mName = name;
     }
 
     public String getProfilePath() {
         return mProfilePath;
     }
 
-    public void setProfilePath(String mProfilePath) {
-        this.mProfilePath = mProfilePath;
+    public void setProfilePath(String profilePath) {
+        this.mProfilePath = profilePath;
     }
 
     public int getId() {
         return mId;
     }
 
-    public void setId(int mId) {
-        this.mId = mId;
+    public void setId(int id) {
+        this.mId = id;
+    }
+
+    public boolean isAdult() {
+        return mAdult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.mAdult = adult;
+    }
+
+    public String getBiography() {
+        return mBiography;
+    }
+
+    public void setBiography(String biography) {
+        this.mBiography = biography;
+    }
+
+    public Date getBirthday() {
+        return mBirthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.mBirthday = birthday;
+    }
+
+    public Date getDeathday() {
+        return mDeathday;
+    }
+
+    public void setDeathday(Date deathday) {
+        this.mDeathday = deathday;
+    }
+
+    public Gender getGender() {
+        return mGender;
+    }
+
+    public void setGender(Gender gender) {
+        this.mGender = gender;
+    }
+
+    public String getImdbId() {
+        return mImdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.mImdbId = imdbId;
+    }
+
+    public String getPlaceOfBirth() {
+        return mPlaceOfBirth;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.mPlaceOfBirth = placeOfBirth;
+    }
+
+    public Double getPopularity() {
+        return mPopularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.mPopularity = popularity;
     }
 }
