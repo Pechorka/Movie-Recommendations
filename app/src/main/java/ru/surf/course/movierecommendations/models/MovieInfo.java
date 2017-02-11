aefbe9231e1712ae45bdba0e2813b866db7c4fd6
nadraliev <nadraliev@gmail.com>
2017-02-11 18:40:40 +0300

    changed fillFields to search superclass too

--- app/src/main/java/ru/surf/course/movierecommendations/models/MovieInfo.java
+++ app/src/main/java/ru/surf/course/movierecommendations/models/MovieInfo.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
@@ -68,16 +69,35 @@ public class MovieInfo extends Media implements Serializable{
     }
 
     public void fillFields(Object from) {
-        Field[] fields = from.getClass().getDeclaredFields();
+        ArrayList<Field> fields = new ArrayList<>(Arrays.asList(from.getClass().getDeclaredFields()));
+        fields.addAll(Arrays.asList(from.getClass().getSuperclass().getDeclaredFields()));
         for (Field field : fields) {
+            Field fieldFrom = null;
+            Field fieldTo = null;
+            Object value = null;
             try {
-                Field fieldFrom = from.getClass().getDeclaredField(field.getName());
-                Object value = fieldFrom.get(from);
-                this.getClass().getDeclaredField(field.getName()).set(this, value);
-            } catch (IllegalAccessException e) {
-                Log.d(LOG_TAG, "Copy error" + e.getMessage());
+                fieldFrom = from.getClass().getDeclaredField(field.getName());
             } catch (NoSuchFieldException e) {
-                Log.d(LOG_TAG, "Copy error" + e.getMessage());
+                try {
+                    fieldFrom = from.getClass().getSuperclass().getDeclaredField(field.getName());
+                } catch (NoSuchFieldException secondE) {
+                    Log.d(LOG_TAG, "Copy error " + e.getMessage());
+                }
+            }
+            try {
+               fieldTo = this.getClass().getDeclaredField(field.getName());
+            } catch (NoSuchFieldException e) {
+                try {
+                    fieldTo = this.getClass().getSuperclass().getDeclaredField(field.getName());
+                } catch (NoSuchFieldException secondE) {
+                    Log.d(LOG_TAG, "Copy error " + e.getMessage());
+                }
+            }
+            try {
+                value = fieldFrom.get(from);
+                fieldTo.set(this, value);
+            } catch (IllegalAccessException e) {
+                Log.d(LOG_TAG, "Copy error " + e.getMessage());
             }
         }
     }
